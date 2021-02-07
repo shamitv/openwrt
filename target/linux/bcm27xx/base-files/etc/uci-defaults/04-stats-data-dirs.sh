26301d05dbe63bf5c7d368f67207dd02888a6dd7
Shamit Verma <github@shamit.in>
2021-02-07 10:45:55 +0000

    Persist  /data/ directory during upgrades

--- target/linux/bcm27xx/base-files/etc/uci-defaults/04-stats-data-dirs.sh
+++ target/linux/bcm27xx/base-files/etc/uci-defaults/04-stats-data-dirs.sh
@@ -3,4 +3,6 @@
 #Create data dir if it does not exist
 mkdir -p /data
 
+#Add /data/ directory to sysupgrade.conf so tht data is preserved during upgrades
+grep -qxF '/data/' /etc/sysupgrade.conf || echo '/data/' >> /etc/sysupgrade.conf
 ~
